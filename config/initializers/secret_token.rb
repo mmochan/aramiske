6b949e9c7d7fb5b4ab87c30199e0c99e5a6e3d4d
Mike Mochan <mmochan@mac.com>
2012-03-07 21:38:54 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aramiske::Application.config.secret_token = '35fda38055ac61a80b6c0f8341e718e94ddc166a61c2ccc93ac253906097055706634b8fc38a193930e0d5aeea13c807417f6adb322ca9018a7b03120f04a3b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Aramiske::Application.config.session_store :cookie_store, key: '_aramiske_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Aramiske::Application.config.session_store :active_record_store
